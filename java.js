266a5e30176e6bb34d7cb7c6fa99f300a5b2f50d
Navaneethn <navaneethnagesh56@gmail.com>
2017-07-28 23:15:11 +0530

    initial commit

---
+++ java.js
@@ -0,0 +1,16 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+$(document).ready(function () {
+    "use strict";
+    $(".fa-times").click(function () {
+        $(".sidebar_menu").addClass("hide_menu");
+        $(".toggle_menu").addClass("opacity_one");
+    });
+
+    $(".toggle_menu").click(function () {
+        $(".sidebar_menu").removeClass("hide_menu");
+        $(".toggle_menu").removeClass("opacity_one");
+
+    });
+
+});
